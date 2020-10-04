3e046b45e9b10dbbc57b2874f2a85f450be56e4b
Guilherme Farias <guilhermefarias.cs@gmail.com>
2020-10-04 20:45:00 -0300

    Meu primeiro projeto em Nodejs

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@
 module.exports = {
     development: {
         database: {
-            host: 'ls-20024416c34ba6dbfa3923d3f3f120e6e3a030c7.ceefo25bexkd.us-east-1.rds.amazonaws.com',
+            host: 'ls-ba0201b9b0d2e9bde1afa3debdbe069404bba679.cpwr4ud8dm7c.us-east-1.rds.amazonaws.com',
             port: 3306,
             name: 'db_usuario',
             dialect: 'mysql',
             user: 'dbmasteruser',
-            password: '-.CCUH04CY5Nt9H?cNb^$,`ertl(5j{R'
+            password: 'lIK9bG;5cGn6$]44>mb,m24w##<I^,b8'
         }
     },
     production: {
